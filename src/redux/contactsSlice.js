12cfef280637e9496b5b6da9ff60e50a438e4322
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-10-06 19:01:37 +0300

    made code refactoring - replace vanilla redux with redux toolkit/add redux-persist logic

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,38 +1,83 @@
 import { nanoid } from 'nanoid';
+import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const contactsInitialState = {
   contacts: [],
 };
 
-export const contactsReducer = (state = contactsInitialState, action) => {
-  switch (action.type) {
-    case 'contacts/addContact': {
-      return { contacts: [...state.contacts, action.payload] };
-    }
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.contacts.push(action.payload);
+      },
+      prepare(newContact) {
+        return {
+          payload: { id: nanoid(), ...newContact },
+        };
+      },
+    },
+    deleteContact: {
+      reducer(state, action) {
+        return {
+          contacts: state.contacts.filter(
+            contact => contact.id !== action.payload
+          ),
+        };
+      },
+      prepare(contactId) {
+        return {
+          payload: contactId,
+        };
+      },
+    },
+  },
+});
 
-    case 'contacts/deleteContact': {
-      return {
-        contacts: state.contacts.filter(
-          contact => contact.id !== action.payload
-        ),
-      };
-    }
+export const { addContact, deleteContact } = contactsSlice.actions;
 
-    default:
-      return state;
-  }
+export const contactsReducer = contactsSlice.reducer;
+
+const persistConfig = {
+  key: 'root',
+  storage,
 };
 
-export const addContact = newContact => {
-  return {
-    type: 'contacts/addContact',
-    payload: { id: nanoid(), ...newContact },
-  };
-};
+export const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
-export const deleteContact = contactId => {
-  return {
-    type: 'contacts/deleteContact',
-    payload: contactId,
-  };
-};
+// export const contactsReducer = (state = contactsInitialState, action) => {
+//   switch (action.type) {
+//     case 'contacts/addContact': {
+//       return { contacts: [...state.contacts, action.payload] };
+//     }
+
+//     case 'contacts/deleteContact': {
+//       return {
+//         contacts: state.contacts.filter(
+//           contact => contact.id !== action.payload
+//         ),
+//       };
+//     }
+
+//     default:
+//       return state;
+//   }
+// };
+
+// export const addContact = newContact => {
+//   return {
+//     type: 'contacts/addContact',
+//     payload: { id: nanoid(), ...newContact },
+//   };
+// };
+
+// export const deleteContact = contactId => {
+//   return {
+//     type: 'contacts/deleteContact',
+//     payload: contactId,
+//   };
+// };
